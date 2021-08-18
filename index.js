59de50797773cdc1272c1a43c819dd8cc58763b8
mamark90 <mamark90@gmail.com>
2021-08-18 17:59:33 -0400

    commit tree 83a717401a683ed32f6a08a528df0f967526421a

--- index.js
+++ index.js
@@ -1,1 +1,70 @@
-// Your code here
+ function createEmployeeRecord(array) {
+ 	debugger;
+ 	const employee = {};
+	  employee["firstName"] = array[0];
+	  employee["familyName"] = array[1];
+	  employee["title"] = array[2];
+	  employee["payPerHour"] = array[3];
+	  employee["timeInEvents"] = [];
+	  employee["timeOutEvents"] = [];
+
+	  return employee;
+ }
+
+function createEmployeeRecords(rows) {
+  let employeeRecord = rows.map(createEmployeeRecord);
+  return employeeRecord;
+}
+
+function createTimeInEvent(employee, date) {
+  let eeTimeData = {};
+  eeTimeData["type"] = "TimeIn";
+  eeTimeData["hour"] = parseInt(date.split(" ")[1]);
+  eeTimeData["date"] = date.split(" ")[0];
+
+  employee.timeInEvents.push(eeTimeData);
+  return employee;
+}
+
+function createTimeOutEvent(employee, date) {
+  let eeTimeData = {};
+  eeTimeData["type"] = "TimeOut";
+  eeTimeData["hour"] = parseInt(date.split(" ")[1]);
+  eeTimeData["date"] = date.split(" ")[0];
+
+  employee.timeOutEvents.push(eeTimeData);
+  return employee;
+}
+
+function hoursWorkedOnDate(employee, date) {
+
+ let timeOut = employee.timeOutEvents.find(function(e){
+   e.date === date;
+   return e.hour;
+ });
+
+  let timeIn = employee.timeInEvents.find(function(e){
+   e.date === date;
+   return e.hour;
+ });
+
+  let diff = (timeOut.hour - timeIn.hour) / 100;
+  return diff;
+}
+
+function wagesEarnedOnDate(employee, date) {
+  let hours = hoursWorkedOnDate(employee, date);
+  return employee["payPerHour"] * hours;
+}
+
+function allWagesFor(employee) {
+let wageArray = [];
+  for (const timeIn of employee.timeInEvents) {
+    debugger;
+    //console.log(employee);
+     let date = timeIn.date;
+     let wage = wagesEarnedOnDate();
+     console.log(wage);
+   }
+    //console.log(wageArray);
+}
